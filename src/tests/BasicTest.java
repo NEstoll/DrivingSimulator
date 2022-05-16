d0d041bb6a4843bf8a9bddafce6ca513ae254d79
Nicholas Estoll <nicholas.estoll@gmail.com>
2022-05-16 14:41:19 -0600

    Initial commit

---
+++ src/tests/BasicTest.java
@@ -0,0 +1,13 @@
+package tests;
+
+import application.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
+
+public class BasicTest {
+    @Test
+    void testMain() {
+        Main.main(null);
+    }
+}
